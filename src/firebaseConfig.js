4fb8c859eb4b4c0c843e853f995ad833cf953736
Bijay Jena <bijay.jena@squbix.com>
2024-06-12 18:27:08 +0530

    env added

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,19 +1,15 @@
-// Import the functions you need from the SDKs you need
-import {initializeApp} from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBhI5ViKKl-ENbj_XXEJ711ES2YJmmrgDg",
+    apiKey: import.meta.env.VITE_FIREBASE_KEY,
     authDomain: "squbix-wfh.firebaseapp.com",
     projectId: "squbix-wfh",
     storageBucket: "squbix-wfh.appspot.com",
     messagingSenderId: "263734908902",
-    appId: "1:263734908902:web:10e43ae7ff4022246ca827"
+    appId: "1:263734908902:web:10e43ae7ff4022246ca827",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export {app};
+export { app };
